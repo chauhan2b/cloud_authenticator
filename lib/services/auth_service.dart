bbee37b2d2cc5c455e85884078a146c706a35754
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-02-28 14:28:28 -0500

    Add AuthService class for authentication

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,68 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../models/auth/user_model.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // convert firebase user to custom user model
+  UserModel? _userFromFirebaseUser(User? user) {
+    return user != null ? UserModel(uid: user.uid, email: user.email!) : null;
+  }
+
+  // auth change user stream
+  Stream<UserModel?> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+
+  // create a new account with email and password
+  Future<UserModel?> createAccount(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } on FirebaseAuthException catch (error) {
+      switch (error.code) {
+        case 'weak-password':
+          throw Exception('The password provided is too weak.');
+        case 'email-already-in-use':
+          throw Exception('The account already exists for that email.');
+        default:
+          throw Exception('Error: ${error.message}');
+      }
+    } catch (error) {
+      throw Exception('Error: $error');
+    }
+  }
+
+  // sign in with email and password
+  Future<UserModel?> signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } on FirebaseAuthException catch (error) {
+      switch (error.code) {
+        case 'user-not-found':
+          throw Exception('No user found for that email.');
+        case 'wrong-password':
+          throw Exception('Wrong password provided for that user.');
+        default:
+          throw Exception('Error: ${error.message}');
+      }
+    } catch (error) {
+      throw Exception('Error: $error');
+    }
+  }
+
+  // sign out
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (error) {
+      throw Exception('Error: $error');
+    }
+  }
+}
