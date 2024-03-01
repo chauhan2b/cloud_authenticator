2a13e14b9c093808e403e8ec4f625c9c2e1a160e
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-03-01 18:35:17 -0500

    Add simple auth text fields and buttons for testing

--- lib/screens/auth/signin_screen.dart
+++ lib/screens/auth/signin_screen.dart
@@ -1,15 +1,78 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cloud_authenticator/providers/auth/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 @RoutePage()
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends ConsumerStatefulWidget {
   const SignInScreen({super.key});
 
   @override
+  ConsumerState<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends ConsumerState<SignInScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Sign In Screen"),
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(label: Text('Email')),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(label: Text('Password')),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(firebaseAuthProvider.notifier)
+                    .signIn('test@test.com', '123456');
+              },
+              child: const Text('Sign in'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(firebaseAuthProvider.notifier)
+                    .createAccount('test@test.com', '123456');
+              },
+              child: const Text('Register'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                final authState = ref.read(authStateProvider);
+                print(authState.value.toString());
+              },
+              child: const Text('Get Auth State'),
+            ),
+          ],
+        ),
       ),
     );
   }
