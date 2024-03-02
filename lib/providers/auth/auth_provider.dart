5dfe8fa452a85c8a1c5f3cd545e581c3c554e568
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-03-02 10:07:30 -0500

    Add auth provider and FirebaseAuth class

---
+++ lib/providers/auth/auth_provider.dart
@@ -0,0 +1,40 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../models/auth/user_model.dart';
+import '../../services/auth_service.dart';
+
+part 'auth_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+AuthService authService(AuthServiceRef ref) {
+  return AuthService();
+}
+
+@Riverpod(keepAlive: true)
+Stream<UserModel?> authState(AuthStateRef ref) {
+  final authService = ref.watch(authServiceProvider);
+  return authService.user;
+}
+
+@riverpod
+class FirebaseAuth extends _$FirebaseAuth {
+  @override
+  FutureOr<void> build() {
+    // nothing to do
+  }
+
+  Future<void> createAccount(String email, String password) async {
+    final authService = ref.watch(authServiceProvider);
+    return await authService.createAccount(email, password);
+  }
+
+  Future<void> signIn(String email, String password) async {
+    final authService = ref.watch(authServiceProvider);
+    return await authService.signIn(email, password);
+  }
+
+  Future<void> signOut() async {
+    final authService = ref.watch(authServiceProvider);
+    return await authService.signOut();
+  }
+}
