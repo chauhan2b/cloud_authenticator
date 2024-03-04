d2c98f18836b2daefbd3c3b9537d49fcaf477547
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-03-04 11:31:57 -0500

    Update HomeScreen to include sign out functionality

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,15 +1,38 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cloud_authenticator/providers/auth/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 @RoutePage()
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
       body: Center(
-        child: Text("Home Screen"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text("Home Screen"),
+            TextButton(
+              onPressed: () async {
+                try {
+                  await ref.read(firebaseAuthProvider.notifier).signOut();
+
+                  if (context.mounted) {
+                    context.router.replaceNamed('/sign-in');
+                  }
+                } catch (error) {
+                  // ignore: avoid_print
+                  print(error);
+                }
+              },
+              child: const Text('Sign Out'),
+            ),
+          ],
+        ),
       ),
     );
   }
