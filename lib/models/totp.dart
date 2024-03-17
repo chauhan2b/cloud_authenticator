82a9a3aeb762bf3c842ed32394729594922e9c5e
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-03-17 13:08:14 -0400

    Add TOTP model class

---
+++ lib/models/totp.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'totp.freezed.dart';
+part 'totp.g.dart';
+
+@freezed
+class TOTP with _$TOTP {
+  const factory TOTP({
+    required String code,
+    required String issuer,
+    required String email,
+  }) = _TOTP;
+
+  factory TOTP.fromJson(Map<String, dynamic> json) => _$TOTPFromJson(json);
+}
