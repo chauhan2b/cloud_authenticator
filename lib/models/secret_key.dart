f63624448a22c0839debf5aa67397d9f81988ce9
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-03-17 13:08:39 -0400

    Add SecretKey model class

---
+++ lib/models/secret_key.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'secret_key.g.dart';
+part 'secret_key.freezed.dart';
+
+@freezed
+class SecretKey with _$SecretKey {
+  const factory SecretKey({
+    required String secret,
+    required String id,
+  }) = _SecretKey;
+
+  factory SecretKey.fromJson(Map<String, dynamic> json) =>
+      _$SecretKeyFromJson(json);
+}
