bf7b6fb51a3ed6e36564a69109d468b502a64d32
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-03-17 22:46:26 -0400

    Add shared preferences support for saving secrets

--- lib/providers/totp/secret_provider.dart
+++ lib/providers/totp/secret_provider.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 import '../../models/secret_key.dart';
 
@@ -11,19 +13,44 @@ class Secret extends _$Secret {
   final _uid = FirebaseAuth.instance.currentUser?.uid;
   final _firestore = FirebaseFirestore.instance;
 
+  // local list of secrets
+  List<SecretKey> _secrets = [];
+
+  // save secrets to shared preferences
+  Future<void> _saveSecretsToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final secretsString = jsonEncode(_secrets.map((e) => e.toJson()).toList());
+    await prefs.setString('secrets', secretsString);
+  }
+
+  // fetch secrets from firestore
   Future<List<SecretKey>> _fetchSecrets() async {
     try {
+      // try loading from shared preferences
+      final prefs = await SharedPreferences.getInstance();
+      final secretsString = prefs.getString('secrets');
+      if (secretsString != null) {
+        final List<dynamic> secretsJson = jsonDecode(secretsString);
+        _secrets = secretsJson.map((e) => SecretKey.fromJson(e)).toList();
+        return _secrets;
+      }
+
+      // if not found in shared preferences, load from firestore
       final snapshot = await _firestore
           .collection('users')
           .doc(_uid)
           .collection('secrets')
           .get();
-      return snapshot.docs
+
+      // update local list
+      _secrets = snapshot.docs
           .map((doc) => SecretKey(
                 secret: doc['secret'] as String,
                 id: doc.id,
               ))
           .toList();
+      await _saveSecretsToPrefs();
+      return _secrets;
     } catch (error) {
       throw Exception('Error fetching secrets');
     }
@@ -35,16 +62,24 @@ class Secret extends _$Secret {
   }
 
   Future<void> addSecret(String secret) async {
+    // add to firestore
     state = const AsyncValue.loading();
-    await _firestore
+    final docRef = await _firestore
         .collection('users')
         .doc(_uid)
         .collection('secrets')
         .add({'secret': secret});
-    state = await AsyncValue.guard(() => _fetchSecrets());
+
+    // add to local list
+    _secrets.add(SecretKey(secret: secret, id: docRef.id));
+
+    // update state
+    state = AsyncValue.data(_secrets);
+    await _saveSecretsToPrefs();
   }
 
   Future<void> removeSecret(String id) async {
+    // remove from firestore
     state = const AsyncValue.loading();
     await _firestore
         .collection('users')
@@ -52,6 +87,12 @@ class Secret extends _$Secret {
         .collection('secrets')
         .doc(id)
         .delete();
-    state = await AsyncValue.guard(() => _fetchSecrets());
+
+    // remove from local list
+    _secrets.removeWhere((secret) => secret.id == id);
+
+    // update state
+    state = AsyncValue.data(_secrets);
+    await _saveSecretsToPrefs();
   }
 }
