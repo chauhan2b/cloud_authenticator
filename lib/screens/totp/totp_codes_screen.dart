4c0fc8051270e564c3d363cf5e239098920bc15b
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-04-06 23:41:29 -0400

    Refactor secret_provider.dart to remove shared preferences usage

--- lib/screens/totp/totp_codes_screen.dart
+++ lib/screens/totp/totp_codes_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cloud_authenticator/providers/totp/timer_state_provider.dart';
 import 'package:cloud_authenticator/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
@@ -39,21 +40,115 @@ class _TOTPCodesState extends ConsumerState<TOTPCodesScreen> {
               )
             : ListView.builder(
                 itemCount: secrets.length,
+                padding: const EdgeInsets.all(8),
                 itemBuilder: (context, index) {
                   final secret = secrets[index];
                   final totp = ref.read(totpProvider(secret.key));
 
-                  return ListTile(
-                    title: Text(totp.code),
-                    subtitle: Text(totp.issuer),
-                    leading: Text(remainingTime),
-                    trailing: IconButton(
-                      onPressed: () {
-                        ref
-                            .read(secretProvider.notifier)
-                            .removeSecret(secret.id);
-                      },
-                      icon: const Icon(Icons.delete, color: Colors.red),
+                  return GestureDetector(
+                    onLongPress: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('Delete secret'),
+                            content: Text(
+                                'Are you sure you want to delete ${totp.issuer}?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  ref
+                                      .read(secretProvider.notifier)
+                                      .removeSecret(secret.key);
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Delete'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.all(8),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                const CircleAvatar(
+                                  child: Icon(Icons.security),
+                                ),
+                                const SizedBox(width: 16),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      totp.issuer,
+                                      style: const TextStyle(fontSize: 16),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      totp.email,
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                                const Spacer(),
+                                Text(
+                                  remainingTime,
+                                  style: const TextStyle(fontSize: 16),
+                                )
+                              ],
+                            ),
+                            const SizedBox(height: 16),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  totp.code,
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    letterSpacing: 28,
+                                    color:
+                                        Theme.of(context).colorScheme.secondary,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.copy,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary),
+                                  onPressed: () {
+                                    // copy the code to the clipboard
+                                    Clipboard.setData(
+                                        ClipboardData(text: totp.code));
+                                    // ScaffoldMessenger.of(context).showSnackBar(
+                                    //   const SnackBar(
+                                    //     content: Text('Copied to clipboard'),
+                                    //   ),
+                                    // );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   );
                 },
@@ -71,7 +166,16 @@ class _TOTPCodesState extends ConsumerState<TOTPCodesScreen> {
         ),
         overlayColor: Colors.black,
         overlayOpacity: 0.5,
+        spaceBetweenChildren: 16,
+        childPadding: const EdgeInsets.all(0),
         children: [
+          SpeedDialChild(
+            child: const Icon(Icons.camera_alt),
+            label: 'Scan QR Code',
+            onTap: () {
+              context.router.push(const QRViewRoute());
+            },
+          ),
           SpeedDialChild(
             child: const Icon(Icons.add),
             label: 'Manually Add',
@@ -103,13 +207,6 @@ class _TOTPCodesState extends ConsumerState<TOTPCodesScreen> {
                   );
                 },
               );
-            },
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.camera_alt),
-            label: 'Scan QR Code',
-            onTap: () {
-              context.router.push(const QRViewRoute());
             },
           ),
         ],
