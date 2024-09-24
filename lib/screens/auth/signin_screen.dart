2f1913a0f2ab3d8341e3068b5f462c52b3c417a9
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-09-24 01:47:47 -0400

    Improve visual elements on login screen

--- lib/screens/auth/signin_screen.dart
+++ lib/screens/auth/signin_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud_authenticator/providers/auth/auth_provider.dart';
+import 'package:cloud_authenticator/providers/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -50,7 +51,9 @@ class _SignInScreenState extends ConsumerState<SignInScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final firebaseAuth = ref.read(firebaseAuthProvider.notifier);
+    final firebaseAuth = ref.watch(firebaseAuthProvider.notifier);
+    final isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
 
     Future<void> signIn(String email, String password) async {
       await firebaseAuth.signIn(email, password);
@@ -103,181 +106,196 @@ class _SignInScreenState extends ConsumerState<SignInScreen> {
     return Scaffold(
       body: Form(
         key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _isSigningIn
-                ? const Text(
-                    'Sign In',
-                    style: TextStyle(fontSize: 32),
-                  )
-                : const Text(
-                    'Register',
-                    style: TextStyle(fontSize: 32),
-                  ),
-            _isSigningIn
-                ? Padding(
-                    padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _isSigningIn
+                    ? const Text(
+                        'Sign In',
+                        style: TextStyle(fontSize: 32),
+                      )
+                    : const Text(
+                        'Register',
+                        style: TextStyle(fontSize: 32),
+                      ),
+                _isSigningIn
+                    ? Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Good to see you again!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: isDarkMode
+                                ? Colors.grey.shade400
+                                : Colors.grey.shade700,
+                          ),
+                        ),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Welcome! Let\'s get you set up.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: isDarkMode
+                                ? Colors.grey.shade400
+                                : Colors.grey.shade700,
+                          ),
+                        ),
+                      ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
                     child: Text(
-                      'Good to see you again!',
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontSize: 16, color: Colors.grey.shade700),
+                      'Email',
+                      style: isEmailFocused
+                          ? TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.bold)
+                          : null,
                     ),
-                  )
-                : Padding(
-                    padding: const EdgeInsets.all(16.0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: TextFormField(
+                    controller: _emailController,
+                    focusNode: _emailFocusNode,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Theme.of(context).colorScheme.primary,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      fillColor: Colors.grey.withOpacity(0.2),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
                     child: Text(
-                      'Welcome! Let\'s get you set up.',
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontSize: 16, color: Colors.grey.shade700),
+                      'Password',
+                      style: isPasswordFocused
+                          ? TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.bold)
+                          : null,
                     ),
                   ),
-            const SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 18.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Email',
-                  style: isEmailFocused
-                      ? TextStyle(
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    focusNode: _passwordFocusNode,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
                           color: Theme.of(context).colorScheme.primary,
-                          fontWeight: FontWeight.bold)
-                      : null,
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: TextFormField(
-                controller: _emailController,
-                focusNode: _emailFocusNode,
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 2,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      fillColor: Colors.grey.withOpacity(0.2),
                     ),
-                    borderRadius: BorderRadius.circular(10),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      } else if (value.length < 6) {
+                        return 'Password must be at least 6 characters long';
+                      }
+                      return null;
+                    },
+                    onFieldSubmitted: (_) {
+                      authenticate(
+                        _emailController.text.trim(),
+                        _passwordController.text.trim(),
+                      );
+                    },
                   ),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 18.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Password',
-                  style: isPasswordFocused
-                      ? TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontWeight: FontWeight.bold)
-                      : null,
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                focusNode: _passwordFocusNode,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 2,
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 56,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await authenticate(
+                          _emailController.text.trim(),
+                          _passwordController.text.trim(),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        foregroundColor:
+                            isDarkMode ? Colors.black : Colors.white,
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        padding: const EdgeInsets.symmetric(horizontal: 32),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: _isSigningIn
+                          ? const Text('Sign In')
+                          : const Text('Register'),
                     ),
-                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  } else if (value.length < 6) {
-                    return 'Password must be at least 6 characters long';
-                  }
-                  return null;
-                },
-                onFieldSubmitted: (_) {
-                  authenticate(
-                    _emailController.text.trim(),
-                    _passwordController.text.trim(),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: SizedBox(
-                width: double.infinity,
-                height: 56,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    await authenticate(
-                      _emailController.text.trim(),
-                      _passwordController.text.trim(),
-                    );
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isSigningIn = !_isSigningIn;
+                    });
                   },
-                  style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    foregroundColor: Colors.white,
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    padding: const EdgeInsets.symmetric(horizontal: 32),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
                   child: _isSigningIn
-                      ? const Text('Sign In')
-                      : const Text('Register'),
+                      ? const Text('Don\'t have an account? Register')
+                      : const Text('Already have an account? Sign in'),
                 ),
-              ),
+              ],
             ),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  _isSigningIn = !_isSigningIn;
-                });
-              },
-              child: _isSigningIn
-                  ? const Text('Don\'t have an account? Register')
-                  : const Text('Already have an account? Sign in'),
-            ),
-          ],
+          ),
         ),
       ),
     );
